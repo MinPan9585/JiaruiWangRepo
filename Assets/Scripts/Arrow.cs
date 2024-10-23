62fb3f5a096d18360a09c3d047d255d931554550
MinPan9585 <94884915+MinPan9585@users.noreply.github.com>
2024-10-23 22:59:21 +0800

    1023

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -14,7 +14,6 @@ public class Arrow : MonoBehaviour
     public GameObject hitGroundVFX;
     bool isFlying = true;
     
-
     void Update()
     {
         if (isFlying)
@@ -23,25 +22,46 @@ public class Arrow : MonoBehaviour
             finalMousePos = new Vector3(mousePosition.x, mousePosition.y, 0);
 
             Vector3 direction = finalMousePos - transform.position;
-            Vector3 finalDir = Vector3.Lerp(direction, lastFrameDir, rotateSpeed);
+            // Move the arrow in the direction of the mouse position
+            transform.Translate(transform.up.normalized * speed * Time.deltaTime, Space.World);
 
-            transform.Translate(finalDir.normalized * speed * Time.deltaTime, Space.World);
+            // Calculate the angle in degrees between the arrow's current direction and the direction to the mouse position
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + 90f;
 
-            // Calculate the angle in degrees
-            float angle = Mathf.Atan2(finalDir.y, finalDir.x) * Mathf.Rad2Deg - 90f;
+            // Get the current angle of the arrow
+            float currentAngle = transform.rotation.eulerAngles.z;
 
-            // Apply the rotation to the arrow
-            transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+            // Calculate the difference between the current angle and the target angle
+            float angleDifference = Mathf.DeltaAngle(currentAngle, angle);
 
-            lastFrameDir = finalDir;
+            // Determine the rotation direction and apply the rotation
+            if (angleDifference > 0)
+            {
+                transform.Rotate(0, 0, -rotateSpeed * Time.deltaTime);
+            }
+            else if (angleDifference < 0)
+            {
+                transform.Rotate(0, 0, rotateSpeed * Time.deltaTime);
+            }
+
+
+
+            //Vector3 finalDir = Vector3.Lerp(direction, lastFrameDir, rotateSpeed);
+
+            //transform.Translate(finalDir.normalized * speed * Time.deltaTime, Space.World);
+
+            //// Calculate the angle in degrees
+            //float angle = Mathf.Atan2(finalDir.y, finalDir.x) * Mathf.Rad2Deg - 90f;
+
+            //// Apply the rotation to the arrow
+            //transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+
+            //lastFrameDir = finalDir;
         }
         else
         {
             return;
         }
-        
-
-        
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
