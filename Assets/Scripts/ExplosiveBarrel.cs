5bfb59986d3e42bd51032ac1f4de8dc1901e8d99
MinPan9585 <94884915+MinPan9585@users.noreply.github.com>
2024-12-29 14:53:59 +0800

    1229

--- Assets/Scripts/ExplosiveBarrel.cs
+++ Assets/Scripts/ExplosiveBarrel.cs
@@ -6,23 +6,20 @@ public class ExplosiveBarrel : MonoBehaviour
 {
     private Rigidbody2D rb2d;
     private Animator anim;
+    public PolygonCollider2D poly;
 
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        //poly = GetComponentInChildren<PolygonCollider2D>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "Arrow")
+        if (collision.gameObject.tag == "Arrow")
         {
             Explode();
         }
@@ -30,11 +27,29 @@ public class ExplosiveBarrel : MonoBehaviour
 
     void Explode()
     {
+        GetObjectsInsidePolygon();
         anim.SetTrigger("Explode");
     }
 
     void DestroyBarrel()
     {
         Destroy(gameObject);
+    }
+
+    void GetObjectsInsidePolygon()
+    {
+        Collider2D[] results = new Collider2D[10];
+        ContactFilter2D filter = new ContactFilter2D();
+        filter.useTriggers = true;
+
+        // Set the layer mask to filter specific layers
+        filter.SetLayerMask(LayerMask.GetMask("Enemy"));
+
+        int colliderCount = Physics2D.OverlapCollider(poly, filter, results);
+        for (int i = 0; i < colliderCount; i++)
+        {
+            //Debug.Log(results[i].name);
+            results[i].transform.GetChild(1).GetComponent<Animator>().SetTrigger("Die");
+        }
     }
 }
